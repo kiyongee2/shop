230cc093eb8c8f9883c22addcafed53f02f9c8b0
kiyongee2 <kiyongee2@gmail.com>
2022-11-08 09:51:36 +0900

    board

--- src/main/java/com/shop/config/SecurityConfig.java
+++ src/main/java/com/shop/config/SecurityConfig.java
@@ -28,7 +28,8 @@ public class SecurityConfig{
 
         http.authorizeRequests()
                 .mvcMatchers("/css/**", "/js/**", "/img/**").permitAll()
-                .mvcMatchers("/", "/members/**", "/item/**", "/images/**").permitAll()
+                .mvcMatchers("/", "/members/**", "/item/**", 
+                		"/images/**", "/board/**").permitAll()
                 .mvcMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated();
 
